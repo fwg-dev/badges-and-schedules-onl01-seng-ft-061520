3985e5b0ed67c3e339e39a249266461565cbe99d
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-27 16:20:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,18 +2,18 @@
 def badge_maker(name) #creating a method for speaker
   "Hello, my name is #{name}."
 end
-#
+
 def batch_badge_creator(attendees)
  attendees.collect {|name| "Hello, my name is #{name}."}
 end
-#
-# def assign_rooms(speakers)
-#   room = 0
-#   speakers.collect do |name|
-#     room += 1
-#   "Hello, #{name}! You'll be assigned to room #{room}!"
-# end
-# end
+
+def assign_rooms(attendees)
+  room = 0
+  attendees.collect do |name|
+    room += 1
+  "Hello, #{name}! You'll be assigned to room #{room}!"
+end
+end
 #
 # def printer(speakers)
 #   batch_badge_creator(speakers).each do |value|
